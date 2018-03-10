266cfb6d0e3af43e6daacf57f88aab23beedc1db
aung <s1@invalid.com>
2018-03-09 17:16:55 -0800

    added mongodb instructrions

---
+++ mongodb.md
@@ -0,0 +1,12 @@
+First, install mongodb server
+
+create directory /data/db
+run as admin/root
+`mongod`
+
+go to our Club-Portal/
+create survey directory
+```npm init
+npm install express ejs nodemon mongoose
+atom server.js
+```
---
+++ survey/server.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+    path = require("path"),
+    bodyParser = require('body-parser'),
+    app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, "./static")));
+app.set('views', path.join(__dirname, "./views"));
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res) => {
+  res.render("index");
+});
+
+require('./routes/index.js')(app);
+
+app.listen(8000, () => {
+  console.log("Port 8000");
+});
